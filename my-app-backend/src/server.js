a2d10ce4c07093cfe917f025af2bec164722f5b6
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-27 18:52:13 +0530

    Added JSON format to backend.

--- my-app-backend/src/server.js
+++ my-app-backend/src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get(
-    '/lyros',(req,res) =>{
-        res.send("Hello Backend!!!");
-    });
+app.get("/lyros", (req, res) => {
+  res.send("Hello Backend!!!");
+});
+
+app.listen(8000,()=>{
+    console.log("Listening ")
+}) 
