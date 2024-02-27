7e70fa39471ce719aa17610e47b86dce5ae219c7
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-27 15:25:31 +0530

    Created AricleList re-usable component.

--- my-app/src/pages/ArticlesListPage.js
+++ my-app/src/pages/ArticlesListPage.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
 import articles from "./articles-content";
 
 
@@ -8,13 +8,7 @@ function ArticlesListPage(){
     return(
     <>
     <h1> List of Articles</h1>
-    {articles.map((article)=>(
-        <Link key ={article.name} className = "article-list-item"
-to={`/articles/${article.name}`}>
-    <h3>{article.title}</h3>
-    <p>{article.content[0].substring(0,100)}...</p>
-</Link>
-    ))}
+   <ArticlesList articles = {articles}></ArticlesList>
     </>     
     );
 }
