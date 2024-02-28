c7835c9305581d11bae9f87472fd935ad001b754
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-28 16:08:32 +0530

    Created Backend folder,Created server API,Created dummy db articleInfo.Added Likes count by using PUT method in ExpressJS.

--- my-app-backend/src/server.js
+++ my-app-backend/src/server.js
@@ -1,12 +1,35 @@
 import express from "express";
 
+let articleInfo =[
+  {
+     name: 'split-js',
+     likes: 0
+  },
+  {
+    name: 'map-js',
+    likes: 0
+ },
+ {
+  name: 'filter-js',
+  likes: 0
+}
+];
+
 const app = express();
 app.use(express.json());
 
-app.get("/lyros", (req, res) => {
-  res.send("Hello Backend!!!");
+app.put("/api/articles/:name/likes", (req, res) => {
+ const { name } = req.params;
+ const article = articleInfo.find(article => article.name === name);
+ if(article){
+  article.likes +=1;
+  res.send(`The article ${article.name} has ${article.likes} likes!`)
+ }
+ else{
+  res.send(`Article not Found`);
+ }
 });
 
 app.listen(8000,()=>{
     console.log("Listening ")
-}) 
+});
