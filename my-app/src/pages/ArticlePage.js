af7ccb1ba7fe8c3bb3d74ccca670abbc9f300870
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-03-20 16:12:40 +0530

    Created a custom hook useUser and imported to Article page and changed like and comment code with authentication.

--- my-app/src/pages/ArticlePage.js
+++ my-app/src/pages/ArticlePage.js
@@ -1,14 +1,16 @@
 import articles from "./articles-content";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
 import AddCommentForm from "../components/AddCommentForm";
 import CommentList from "../components/CommentList";
+import useUser from "../hooks/useUser";
 
 function ArticlePage() {
   const [articleInfo, setArticleInfo] = useState({ likes: 0, comments: [] });
   const { articleId } = useParams();
+  const { user, isLoading } = useUser();
 
   useEffect(() => {
     const loadArticleInfo = async () => {
@@ -19,20 +21,26 @@ function ArticlePage() {
     loadArticleInfo();
   }, []);
 
-  const article = articles.find(article => article.name === articleId);
+  const article = articles.find((article) => article.name === articleId);
 
   const addLikes = async () => {
     const response = await axios.put(`/api/articles/${articleId}/likes`);
     const updatedArticle = response.data;
     setArticleInfo(updatedArticle);
-  };  
-  
+  };
+
   if (article) {
     return (
       <>
         <h1>{article.title}</h1>
         <div className="upvote-section">
-          <button onClick={addLikes}>Like</button>
+          {user ? (
+            <button onClick={addLikes}>Like</button>
+          ) : (
+            <Link to="/login">
+              <button>Log in to like</button>
+            </Link>
+          )}
         </div>
         <p>
           {article.title} has {articleInfo.likes} likes!...
@@ -40,10 +48,18 @@ function ArticlePage() {
         {article.content.map((paragraph, i) => (
           <div key={i}>{paragraph}</div>
         ))}
-        <AddCommentForm
-          articleName={articleId}
-          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
-        ></AddCommentForm>
+        {user ? (
+          <AddCommentForm
+            articleName={articleId}
+            onArticleUpdated={(updatedArticle) =>
+              setArticleInfo(updatedArticle)
+            }
+          ></AddCommentForm>
+        ) : (
+          <Link to="/login">
+            <button>Log in to comment</button>
+          </Link>
+        )}
 
         <CommentList comments={articleInfo.comments}></CommentList>
       </>
